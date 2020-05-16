df656b46ee636070f9ea89649873db43e043d80d
Alex Kapelyukhovsky <alkapa2005@gmail.com>
2020-05-16 19:51:33 +0300

    User state to update component

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 const rootElement = document.getElementById("app");
 
-// const myElement = document.createElement("h1");
-// myElement.className = "orange";
-// myElement.innerText = "Hello From Pluralsight";
-
-// const myReactElement = React.createElement(
-//   "h1",
-//   { className: "orange" },
-//   "Hello From Pluralsight and React"
-// );
-
 class Hello extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date().toLocaleString(),
+    };
+  }
+
+  tick() {
+    this.setState(() => ({ time: new Date().toLocaleString() }));
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  componentDidUnmount() {
+    clearInterval(this.interval);
+  }
+
   render() {
     return React.createElement(
       "h1",
       { className: "orange" },
-      "Hello From Pluralsight and React Component " + this.props.time
+      "Hello From Pluralsight and React Component " + this.state.time
     );
   }
 }
 
-//rootElement.appendChild(myElement);
 ReactDOM.render(
-  React.createElement(Hello, { time: new Date().toLocaleDateString() }, null),
+  React.createElement(Hello, {}, null),
   document.getElementById("app")
 );
