e2b10bd8762d0e7584e4b82c2586eda0ebfa1a68
Alex Kapelyukhovsky <alkapa2005@gmail.com>
2020-05-16 19:14:56 +0300

    Render React component

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ const rootElement = document.getElementById("app");
 // myElement.className = "orange";
 // myElement.innerText = "Hello From Pluralsight";
 
-const myReactElement = React.createElement(
-  "h1",
-  { className: "orange" },
-  "Hello From Pluralsight and React"
-);
+// const myReactElement = React.createElement(
+//   "h1",
+//   { className: "orange" },
+//   "Hello From Pluralsight and React"
+// );
+
+const Hello = function () {
+  return React.createElement(
+    "h1",
+    { className: "orange" },
+    "Hello From Pluralsight and React Component"
+  );
+};
 
 //rootElement.appendChild(myElement);
-ReactDOM.render(myReactElement, document.getElementById("app"));
+ReactDOM.render(
+  React.createElement(Hello, {}, null),
+  document.getElementById("app")
+);
