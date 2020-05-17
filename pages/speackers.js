18edbc2d341053c67c6b3ed933d8e4980bc18be7
Alex Kapelyukhovsky <alkapa2005@gmail.com>
2020-05-17 18:01:12 +0300

    rename index to speackers and introduce new static index

---
+++ pages/speackers.js
@@ -0,0 +1,50 @@
+import React from "react";
+import axios from "axios";
+import Link from "next/link";
+
+export default class Speackers extends React.Component {
+  static async getInitialProps() {
+    const promise = axios
+      .get("http://localhost:4000/speakers")
+      .then((response) => ({
+        hasErrored: false,
+        speakerData: response.data,
+      }))
+      .catch((error) => ({
+        hasErrored: true,
+        message: error.message,
+      }));
+
+    return promise;
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasErrored: props.hasErrored,
+      message: props.message,
+      speakerData: props.speakerData,
+    };
+  }
+
+  componentDidMount() {}
+
+  componentUnmount() {}
+
+  render() {
+    return (
+      <div>
+        <Link href="/sessions">
+          <a>Sessions</a>
+        </Link>
+        <ul>
+          {this.state.speakerData.map((speaker) => (
+            <li key={speaker.id}>
+              {speaker.firstName} {speaker.lastName}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
